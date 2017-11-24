d666f1d60b2ded3e357923a2267eaa1ed6912933
pabloram232 <pablo.ram232@gmail.com>
2017-11-23 21:25:12 -0600

    Add files via upload

---
+++ Patrones de diseño/behavioral patterns/JPRI_iterator.py
@@ -0,0 +1,23 @@
+#Juan Pablo Ramirez Ibarra ---- GITI9072
+def count_to(count):
+    """Our iterator implementation"""
+
+    # Our list
+    numbers_in_german = ["eins", "zwei", "drei", "vier", "funf"]
+
+    # Our built-in iterator
+    # Creates a tuple such as (1, "eins")
+    iterator = zip(range(count), numbers_in_german)
+
+    # Iterator trough our iterable list
+    # Extract the German numbers
+    # Put them in a generator called number
+    for position, number in iterator:
+    # Return a "Generator" containing numbers in German
+        yield number
+
+# Let's thest the generator returned by our iterator
+for num in count_to(3):    print("{}".format(num))
+for num in count_to(4):
+    print("{}".format(num))
+
